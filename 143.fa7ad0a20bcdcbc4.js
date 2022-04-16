b3c5dc8074b9d9583377ce38e6929c7a30bf52ea
Brett B <bbarnow17@gmail.com>
2022-04-16 00:57:48 -0600

    Auto-generated commit [ci skip]

---
+++ 143.fa7ad0a20bcdcbc4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkLA_Site=self.webpackChunkLA_Site||[]).push([[143],{6143:(M,E,n)=>{n.r(E),n.d(E,{EngravingsModule:()=>_});var u=n(9808),o=n(7184),s=n(2382),l=n(1223);let _=(()=>{class a{}return a.\u0275fac=function(r){return new(r||a)},a.\u0275mod=l.oAB({type:a}),a.\u0275inj=l.cJS({imports:[[u.ez,o.K6A,s.u5]]}),a})()}}]);
